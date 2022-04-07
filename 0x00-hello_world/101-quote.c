f238a4a9add2c25b67588bb3d190ab721f2f2219
Grace <estyuju@gmail.com>
2022-04-07 14:43:19 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+fprintf("\"and that piece of art is useful" - Dora Korpar, 2015-10-19,");
+return (0);
+}
