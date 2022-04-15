3ae1f59606865e8822f5a164fd554faee3174462
Grace <estyuju@gmail.com>
2022-04-15 01:40:17 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 /**
  * print most_numbers -prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
+ * Return: Always 0
  */
 void print_most_numbers(void)
 {
@@ -14,6 +15,5 @@ void print_most_numbers(void)
 			_putchar(i + '0');
 		}
 	}
-	
 	_putchar('\n');
 }
