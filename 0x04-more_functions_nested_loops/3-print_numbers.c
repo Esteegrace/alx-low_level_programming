4d27632086d29d1bc28c6c849eaa721e8325d6c2
Grace <estyuju@gmail.com>
2022-04-15 01:29:22 -0700

    Fixed print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the number zero to nine
- *
- * Return: Always 0 (Success)
+ * print_numbers - prints the numbers, from 0 to 9,
+ *followed by a new line
  */
 
 void print_numbers(void)
@@ -11,11 +10,10 @@ void print_numbers(void)
 
 int i;
 
-i = 0;
-
 for (i = 0; i < 10; i++)
-
+{
 _putchar(i + '0');
+}
 
 _putchar('\n');
 }
